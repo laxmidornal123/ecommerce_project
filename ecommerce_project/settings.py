27f10962f51647462167c3f7c288ea2901897c7d
Laxmi Dornal <laxmidornal12@gmail.com>
2026-06-07 18:34:01 +0530

    Added invoice PDF and improved cart UI

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -80,3 +80,4 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
